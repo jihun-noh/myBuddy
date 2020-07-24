3cbeed3550fab76c8a4452fa744052e87277794a
jihun-noh <mon772@naver.com>
2020-07-24 19:19:41 +0900

    스태틱파일 관련 수정

--- MyBuddy/settings.py
+++ MyBuddy/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '/front_test/static/',
+    'front_test/static/',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
