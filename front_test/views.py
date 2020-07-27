7ee26d8326a3fe09099eae0fa2cfc7db5cb362b9
jihun-hon <mon772@naver.com>
2020-07-27 18:31:01 +0900

    원복

--- front_test/views.py
+++ front_test/views.py
@@ -10,34 +10,30 @@ def login(request):
 
 def menu(request):
     if request.session.get('email', False):
-        static_root = settings.STATIC_ROOT_WEB
         session_email = request.session['email']
         session_id = request.session['_auth_user_id']
         print('login user : ' + session_email)
         return render(request, 'front_test/menu.html', \
-        {'static_root':static_root, 'session_email':session_email, 'session_id':session_id})
+        {'session_email':session_email, 'session_id':session_id})
     else:
         return redirect('/front/login/')
 
 def map(request):
     if request.session.get('email', False):
-        static_root = settings.STATIC_ROOT_WEB
         app_key = settings.KAKAO_JAVASCRIPT_APP_KEY
-        return render(request, 'front_test/map.html', {'static_root':static_root, 'app_key': app_key})
+        return render(request, 'front_test/map.html', {'app_key': app_key})
     else:
         return redirect('/front/login/')
 
 def dive_log_list(request):
     if request.session.get('email', False):
-        static_root = settings.STATIC_ROOT_WEB
-        return render(request, 'front_test/diveloglist.html', {'static_root':static_root})
+        return render(request, 'front_test/diveloglist.html')
     else:
         return redirect('/front/login/')
 
 def dive_log_form(request):
     if request.session.get('email', False):
-        static_root = settings.STATIC_ROOT_WEB
-        return render(request, 'front_test/divelogform.html', {'static_root':static_root})
+        return render(request, 'front_test/divelogform.html')
     else:
         return redirect('/front/login/')
 
